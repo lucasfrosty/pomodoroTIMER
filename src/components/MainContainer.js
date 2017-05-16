1adfe421948fe966b69be5298a8c12d042bb2ef6
lucasfrosty <lucasfr@cc.ci.ufpb.br>
2017-05-16 03:55:57 -0300

    notification settings almost finished and favicon updated

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -25,7 +25,8 @@ class MainContainer extends React.Component {
     this.resetTime = this.resetTime.bind(this);
     this.pauseTime = this.pauseTime.bind(this);
     this.convertTime = this.convertTime.bind(this);
-    this.handleStartClick = this.handleStartClick.bind(this);
+    this.updateTimeEverySecond = this.updateTimeEverySecond.bind(this);
+    this.creatingNotification = this.creatingNotification.bind(this);
 
   }
 
@@ -45,13 +46,11 @@ class MainContainer extends React.Component {
   updateTime() {
     if (this.state.time === 0) { // entries the loop if the time is over
       this.setState({
-        time: this.getTime(this.state.pomodoroCounter + 1),
-        pomodoroCounter: (this.state.pomodoroCounter + 1) % 8
-
         /* was necessary to use pomodoroCounter + 1 here because the getTime
         would've return the time based on the previous counter since i didn't
         incremented that yet */
-
+        pomodoroCounter: (this.state.pomodoroCounter + 1) % 8,
+        time: this.getTime(this.state.pomodoroCounter + 1)
       });
       this.pauseTime();
     } else {
@@ -64,33 +63,17 @@ class MainContainer extends React.Component {
         });
       }
     }
-
-
   }
 
-  playSound() {
-    let audio = new Audio(this.props.base64url);
-    audio.play();
-  }
-
-  // code get from mozilla documentation
-  sendNotification() {
-    // Let's check if the browser supports notifications
-    if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
-    } else if (Notification.permission === "granted") { // notification permissions have already been granted
-      new Notification("Time's up!!");
-    } else if (Notification.permission !== 'denied') { // Otherwise, we need to ask the user for permission
-      Notification.requestPermission((permission) => {
-        // If the user accepts, let's create a notification
-        if (permission === "granted") {
-          new Notification("Time's up!!");
-        }
-      });
+  updateTimeEverySecond() {
+    /* this function will check if the clock is already running,
+    in case is running, the updateTimeEverySecond function will not be called again */
+    if(this.state.isRunning) {
+      alert('The clock is running already');
+    } else {
+      this.timerID = setInterval(this.updateTime, 1000);
     }
   }
-
-  updateTimeEverySecond = () => this.timerID = setInterval(this.updateTime, 1000);
 
   pauseTime () {
     this.setState({
@@ -135,27 +118,63 @@ class MainContainer extends React.Component {
     }
   }
 
-  handleStartClick() {
-    if(this.state.isRunning) {
-      alert('The clock is running already');
-    } else {
-      this.updateTimeEverySecond();
+  playSound() {
+    /* i'm using the sound on a base64 data URI, so i chose to pass the data uri
+    as props because the string was really huge */
+    let audio = new Audio(this.props.base64url);
+    audio.play();
+  }
+
+  isWorkTime = () => !this.state.pomodoroCounter % 2;
+
+  creatingNotification () {
+
+    let notificationSettings = {
+      title: this.isWorkTime() ? 'STOP CODING!' : "LET'SS CODE!!11",
+      body: this.isWorkTime() ? "You have a 5 minutes break, you must not do anything about the work." : 'Your rest time is over, you have 25 minutes to do your stuffs. Try to not get distracted and focus on your work.',
+      icon: this.isWorkTime() ? 'https://maxcdn.icons8.com/Color/PNG/96/Transport/stop_sign-96.png' : 'https://maxcdn.icons8.com/Color/PNG/96/Transport/go-96.png'
+    }
+
+    new Notification(notificationSettings.title, {
+      icon: notificationSettings.icon,
+      body: notificationSettings.body
+    })
+  }
+
+  // code get from mozilla official documentation
+  sendNotification() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    } else if (Notification.permission === "granted") { // notification permissions have already been granted
+      this.creatingNotification();
+    } else if (Notification.permission !== 'denied') { // Otherwise, we need to ask the user for permission
+      Notification.requestPermission((permission) => {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          this.creatingNotification();
+        }
+      });
     }
   }
 
   render() {
+    let timeInMinutes = this.convertTime(this.state.time);
+
+    // sending notification if the time is over.
     if (this.state.time === 0) {
       this.playSound();
       this.sendNotification();
     }
 
-    let timeInMinutes = this.convertTime(this.state.time);
+    // setting the page time to 'MM:SS - pomodoroTIMER'
     this.setPageTitle(timeInMinutes + ' - pomodoroTIMER');
+
     return (
       <div>
         <HeaderIcon pomodoroCounter={this.state.pomodoroCounter}/>
         <Clock>{timeInMinutes}</Clock>
-        <ButtonContainer updateTimeEverySecond={this.handleStartClick}
+        <ButtonContainer updateTimeEverySecond={this.updateTimeEverySecond}
           pauseTime={this.pauseTime}
           refreshTime={this.refreshTime}
           resetTime={this.resetTime}
